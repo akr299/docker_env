f929b2911e99bf5b35ec0f52176c7cbb6fb5dea8
akr299 <akira.ohira@el-ltd.co.jp>
2024-09-21 22:12:48 +0900

    20240921

--- localstack環境構築/scripts/init.sh
+++ localstack環境構築/scripts/init.sh
@@ -5,10 +5,10 @@ awslocal s3 cp sample.txt s3://test-bucket/
 awslocal s3 ls s3://test-bucket
 apt update
 apt -y install maven
-npm i -g serverless
-npm i -D serverless-localstack
-#after_create_project
-npm install serverless-localstack --save-dev 
-# deploy
-# mvn package
-# serverless deploy
+# npm i -g serverless
+# npm i -D serverless-localstack
+# #after_create_project
+# npm install serverless-localstack --save-dev 
+# # deploy
+# # mvn package
+# # serverless deploy
