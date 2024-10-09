2430f8c5c78dd77ed31b72c50d74abc78c7df45d
akr299 <akira.ohira@el-ltd.co.jp>
2024-10-09 22:17:57 +0900

    20241009_まだ

--- localstack環境構築/scripts/init.sh
+++ localstack環境構築/scripts/init.sh
@@ -8,7 +8,10 @@ apt -y install maven
 # npm i -g serverless
 # npm i -D serverless-localstack
 # #after_create_project
+# aws configure --profile localstack
 # npm install serverless-localstack --save-dev 
+# # cd /opt/sls/aws-python-rest-api-with-dynamodb
 # # deploy
 # # mvn package
+# # serverless login
 # # serverless deploy
